56ecef64b947a69eb5f43ffe5c438b5b3847cc36
dilen shankar <elchurrocuandotebese@gmail.com>
2025-03-18 21:09:56 +0530

    Update create-features.py

--- create-features.py
+++ create-features.py
@@ -1,7 +1,1 @@
-df['Previous Close'] = df['Close'].shift(1)
-df['Sentiment Score Shifted'] = df['Sentiment Score'].shift(1)
-df['MA_5'] = df['Close'].rolling(window=5).mean()
-df['MA_10'] = df['Close'].rolling(window=10).mean()
- # Drop NaN values
- df.dropna(inplace=True)
- print(df.head()
+
